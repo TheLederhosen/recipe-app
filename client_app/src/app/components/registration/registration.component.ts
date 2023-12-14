e79480b9441ccb52a1b6872b7458003766ce1217
Josef Rabmer <jrabmer1@gmail.com>
2023-12-14 14:04:04 +0100

    Started on password validation

--- client_app/src/app/components/registration/registration.component.ts
+++ client_app/src/app/components/registration/registration.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
-import { GlobalFunctions } from "../../global/globalFunctions";
 import { UserService } from 'src/app/services/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SuccessSnackbarComponent } from '../success-snackbar/success-snackbar.component';
+import { passwordMatchValidator } from 'src/app/global/validators/password-match.validator';
 
 
 @Component({
@@ -27,14 +27,39 @@ export class RegistrationComponent {
       lastname: ['', [Validators.required, Validators.pattern('[a-zA-Z-äöüßÄÖÜ]*'), Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(100)]],
-      confirm: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(100)]],
-    });
+      passwordConfirm: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(100)]],
+    }, { validators: passwordMatchValidator });
+  }
+
+  getErrorMessage(controlName: string, field: string) {
+    if (this.registerForm.controls[controlName].hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    if (this.registerForm.controls[controlName].hasError('email')) {
+      return `Not valid email provided`;
+    }
+
+    if (this.registerForm.controls[controlName].hasError('pattern')) {
+      return `${field} can only include letters and äöü`;
+    }
+
+    if (this.registerForm.controls[controlName].hasError('minlength')) {
+      return `${field} needs to be at least 8 characters`;
+    }
+
+    if (this.registerForm.controls[controlName].hasError('maxlength')) {
+      return `${field} is too long`;
+    }
+
+    return 'no errors';
   }
 
   /**
    * Form validation will start after the method is called, additionally an AuthRequest will be sent
    */
   createNewUser() {
+    // TODO: Notify user when passwords do not match
     if(this.registerForm.valid) {
       const firstName = this.registerForm.controls['firstname'].value;
       const lastName = this.registerForm.controls['lastname'].value;
